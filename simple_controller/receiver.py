a4a38f9d53b1ecb294d4752fd8ff917f3ecc8f80
shunta <shunta.8529@gmail.com>
2025-08-14 14:19:50 +0900

    receiver です

---
+++ simple_controller/receiver.py
@@ -0,0 +1,14 @@
+import socket
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print(f"Listening on UDP port {UDP_PORT}...")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(f"Received message: {data.decode()} from {addr}")
+
